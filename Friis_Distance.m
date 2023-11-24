1b8d0e9e7b33a63a4901424988984abeaa9a4414
SorenBogh <SorenBogh@users.noreply.github.com>
2023-11-25 00:19:08 +0100

    Update Friis_Distance.m
    Made loop for multiple frequencies

--- Friis_Distance.m
+++ Friis_Distance.m
@@ -7,22 +7,27 @@ G_R = 1;
 G_T = 1;
 
 c = 3E8;
-f_c = input("Insert the used frequency in MHz\n");
-lambda = c/(f_c * 1E6);
+
 
 d = 1:1:1E4;
 
-FSPL = ((4*pi*d).^2)/(lambda^2*PLF*G_T*G_R);
+for f_c = 3000:1000:6000
 
-figure(1)
-plot(d, 10*log10(FSPL));
-xlabel("d [m]");
-ylabel("FSPL [dB]");
-title("FSPL vs. distance")
-grid on
+    lambda = c/(f_c * 1E6);
+    
+    FSPL = ((4*pi*d).^2)/(lambda^2*PLF*G_T*G_R);
+    
+    figure(1)
+    plot(d, 10*log10(FSPL));
+    xlabel("d [m]");
+    ylabel("FSPL [dB]");
+    title("FSPL vs. distance")
+    grid on
+    hold on
+end
+hold off
+legend("3GHz", "4GHz", "5GHz", "6GHz", "Location","southeast");
 
 %Skal måske lave det sådan man kan se hvordan ændring i polarisering og
 %gain spiller rolle her.
-%kan også være der skal tilføjes et forloop for at køre med flere
-%frekvenser.
 
